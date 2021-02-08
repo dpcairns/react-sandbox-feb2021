8c26e24361eec3380c4f2ff24503b3fa17e0db12
harriet-the-spy <dpcairns@gmail.com>
2021-02-08 11:53:47 -0800

    add dog List

--- src/App.js
+++ src/App.js
@@ -11,10 +11,56 @@ function add(num1, num2) {
   return num1 + num2
 }
 
+const dogs = [
+  {
+    name: 'spot',
+    age: 3,
+  },
+  {
+    name: 'jeep',
+    age: 2,
+  },
+  {
+    name: 'rover',
+    age: 1,
+  },
+]
+
+class Dog extends React.Component {
+  render() {
+    return <div>
+    <p>My name is {this.props.dog.name}</p>
+    <p>and i am {this.props.dog.age} years old</p>
+  </div>
+  }
+}
+
 export default class App extends React.Component {
   render() {
+    // const dogList = dogs.map(dog => <div>
+    //     <p>My name is {dog.name}</p>
+    //     <p>and i am {dog.age} years old</p>
+    //   </div>
+    // );
+
+    // dogList is an array of Dog components
+    const dogList = 
+      // dogs is an array of dog objects with a name and age
+      dogs.map(
+        // dog is a single dog object with a name and age
+        dog => 
+          // Dog is a react component
+            // dog is a prop name
+          <Dog dog={
+            // dog is the dog object from line 51 that we're passing as a prop
+            dog
+          } />)
+
+    console.log(dogList)
+    
       return (
         <>
+          { dogList }
           <h1 className={style.article}>Time to click the legos together!</h1>
             <MyCoolHeader 
               name="dani" 
