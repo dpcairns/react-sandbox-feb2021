3afa4f5f5db84cf180c43433d79df3c3d2fae0f0
harriet-the-spy <dpcairns@gmail.com>
2021-02-09 11:16:21 -0800

    add dropdown

--- src/App.js
+++ src/App.js
@@ -4,28 +4,56 @@ import './App.css';
 // Import the necessary styles, or include them 
 export default class App extends React.Component {
   state = {
-    // data model for the app
-    someNumber: 0,
-    doubleNumber: 0
-  }
-
-  increment = () => {
-    // what do  i want the next state of someNumber to be?
-    this.setState({ 
-      // key to set
-      someNumber: 
-      // value to set
-      this.state.someNumber + 1,
-    })
+    // 0) initialize some state
+    name: '',
+    flavor: '',
   }
   
+  // 4) in the change handler, grab the event (e)
+  handleNameChange = (e) => {
+    // 5) call this.setState with an object
+    this.setState({ 
+      // 6) the key is the key in state we want to change
+      name: 
+      // 7) the value is the new value from the user that want to set the state to
+      e.target.value // whatever the user has typed
+     });
+  }
+
+  handleVIPClick = () => {
+    this.setState({ name: 'lady gaga' })
+  }
+
   render() {
+    console.log(this.state);
+
       return (
-        <div className="counter">
-          <div>My number: {this.state.someNumber}</div>
-          {/* on click, call this.increment */}
-          <button onClick={this.increment}>Increment</button>
-        </div>
+        <>
+        <form>
+            Name
+            {/* 1) tie the value of the input to the state */}
+            <input 
+              value={this.state.name}
+              // 2) define a change handler for our input
+              onChange={this.handleNameChange} 
+            />
+            Flavor
+            <select
+              value={this.state.flavor}
+              onChange={e => {
+                this.setState({
+                  flavor: e.target.value
+                })
+              }}
+            >
+              <option value="vanilla">Vanilla</option>
+              <option value="chocolate">Chocolate</option>
+              <option value="banana">Banana</option>
+            </select>
+            <button>Submit</button>
+        </form>
+        <button onClick={this.handleVIPClick}>VIP</button>
+        </>
     );
   }
 }
