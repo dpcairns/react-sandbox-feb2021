a420eb68091784197e4dea173e53b7aca134cc6c
harriet-the-spy <dpcairns@gmail.com>
2021-02-10 13:14:36 -0800

    add double filter

--- src/App.js
+++ src/App.js
@@ -32,11 +32,29 @@ export default class App extends React.Component {
     console.log(this.state)
       // this will get called whenever state of props changes
       const filteredIceCreams = iceCreams.filter((iceCream) => {
-        // if there are no flavors selected, show all
-        if (!this.state.flavor) return true; // include this flavor
+        // if there are no flavors AND NO COSTS selected, show all
+        if (!this.state.flavor && !this.state.cost) return true; // include this flavor
 
-        if (iceCream.category === this.state.flavor) return true; // include this flavor
+        // if there is a flavor selected but not cost selected
+        if (this.state.flavor && !this.state.cost) {
+          // only filter by matching flavors
+          if (iceCream.category === this.state.flavor) return true; // include this flavor
+        }
+        
+        // if there is a cost selected but no flavor
+        if (this.state.cost && !this.state.flavor) {
+          // filter based on matching costs
+          if (iceCream.cost === this.state.cost) return true; // include this flavor
+        }
 
+        // if there is both a cost and a flavor selected
+        if (this.state.cost && this.state.flavor) {
+          // filter based on both of these categories  
+          // tech debt: we should use category everywhere since that's the data model
+          if (iceCream.cost === this.state.cost && iceCream.category === this.state.flavor) return true;
+        }
+
+        // if all checks fail, do NOT include this item in our filtered array
         return false; // don't include this flavor
       });
 
